5f7f9bf249a2be478a876b155cf47639a4ad7a66
Peter Grippi <pgrippi@dresourcesgroup.com>
2013-04-23 10:17:54 -0400

    Card colorizer 1.1

--- Trello/card_colorizer.js
+++ Trello/card_colorizer.js
@@ -3,14 +3,51 @@
 // @namespace http://petergrippi.com/
 // @description adds color to cards
 // @match https://trello.com/board/*
+// @version 1.1
 // ==/UserScript==
-(function(){
+(function () {
   "use strict";
 
-  var root = this;
-  var $ = root.jQuery;
-  $('.card-label').each(function(){
-    var $this = $(this);
-    $this.closest('.list-card').css('background-color', $this.css('background-color'));
+  function load(url, onLoad) {
+    var tag = document.createElement("script");
+    tag.setAttribute("src", url);
+    if (typeof (onLoad) === 'function') {
+      tag.addEventListener('load', onLoad);
+    }
+    document.body.appendChild(tag);
+  }
+
+  function execute(functionOrCode) {
+    var code, tag = document.createElement("script");
+
+    if (typeof (functionOrCode) === "function") {
+      code = "(" + functionOrCode + ")()";
+    } else {
+      code = functionOrCode;
+    }
+    tag.textContent = code;
+    document.body.appendChild(tag);
+  }
+
+  function loadAndExecute(url, functionOrCode) {
+    return load(url, function () { return execute(functionOrCode); });
+  }
+
+  loadAndExecute('//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js', function () {
+    var jQuery = window.jQuery.noConflict(true);
+    var $ = jQuery;
+
+    var colorizer = function() {
+      $('.card-label').each(function(){
+        var $this = $(this), data = $this.data();
+        if (!data.colorized) {
+          $this.closest('.list-card').css('background-color', $this.css('background-color'));
+          $this.data('colorized', true);
+        }
+      });
+    };
+
+    $(function() { colorizer(); });
+    setInterval(colorizer, 5000);
   });
-}).call(this);
+}(this));
